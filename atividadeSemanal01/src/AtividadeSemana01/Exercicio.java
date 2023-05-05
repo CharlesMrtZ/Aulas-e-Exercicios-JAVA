5c9fbcddcd162eb8f377e4a2bbb083f38a964d47
Charles Martinazzo <charles.martinazzo@gmail.com>
2023-05-05 17:14:34 -0300

    exercício05

--- atividadeSemanal01/src/AtividadeSemana01/Exercicio.java
+++ atividadeSemanal01/src/AtividadeSemana01/Exercicio.java
@@ -1,18 +1,32 @@
 package AtividadeSemana01;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercicio {
     public static void main(String[] args) {
 
+        ArrayList<String> nomesAlunos = new ArrayList<>();
+
         Scanner sc = new Scanner(System.in);
 
-        String controle = "";
+        boolean controle = true;
 
-        while(!controle.equalsIgnoreCase("fim")){
+        while(controle){
+            String aluno = "";
             System.out.println("Digite o nome do Aluno ou digite [fim] para finalizar");
-            controle =  sc.nextLine();
-            System.out.println(controle);
+            aluno =  sc.nextLine();
+                if (aluno.equalsIgnoreCase("fim")){
+                    controle = false;
+                } else {
+                    nomesAlunos.add(aluno);
+                }
+        }
+
+        System.out.println("LISTA DE ALUNOS:");
+        for (String nome: nomesAlunos) {
+            System.out.println("-----------------------------");
+            System.out.println(nome);
         }
     }
 }
