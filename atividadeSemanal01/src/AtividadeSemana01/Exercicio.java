eaa51b995dd2fc0376bc3a297acea019ec7ea93f
Charles Martinazzo <charles.martinazzo@gmail.com>
2023-05-16 14:18:05 -0300

    exercício06e07

--- atividadeSemanal01/src/AtividadeSemana01/Exercicio.java
+++ atividadeSemanal01/src/AtividadeSemana01/Exercicio.java
@@ -7,19 +7,29 @@ public class Exercicio {
     public static void main(String[] args) {
 
         ArrayList<String> nomesAlunos = new ArrayList<>();
+        ArrayList<Double[]> notasAlunos = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
+        String aluno = "";
+        Double[] notas;
 
-        boolean controle = true;
+        while(!aluno.equalsIgnoreCase("fim")){
+            notas = new Double[3];
 
-        while(controle){
-            String aluno;
             System.out.println("Digite o nome do Aluno ou digite [fim] para finalizar");
             aluno =  sc.nextLine();
-                if (aluno.equalsIgnoreCase("fim")){
-                    controle = false;
-                } else {
+
+                if (!aluno.equalsIgnoreCase("fim")){
                     nomesAlunos.add(aluno);
+
+                    for (int i = 0; i < 3; i++){
+                        System.out.println("Digite a " + (i+1) + "ª nota do aluno " + aluno);
+                        notas[i] = sc.nextDouble();
+                        aluno = sc.nextLine();
+                    }
+
+                notasAlunos.add(notas);
+                    System.out.println(notasAlunos.size());
                 }
         }
 
